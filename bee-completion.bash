7712fbf0f3ca452f5ee97733d32eecf819f045df
Simon Schmid <mail@sschmid.com>
2018-12-09 15:02:24 +0100

    Improve bash-completion

--- bee-completion.bash
+++ bee-completion.bash
@@ -1,9 +1,28 @@
 #!/usr/bin/env bash
 
 _bee_completions() {
-  local wordlist="plugins commands new res version update wiki ❤️"
-  wordlist+=" $(bee commands)"
-  COMPREPLY=($(compgen -W "${wordlist}" "${COMP_WORDS[1]}"))
+  local word="${COMP_WORDS[1]}"
+  if [[ $COMP_CWORD -lt 2 ]]; then
+    local wordlist="help plugins commands new res version update wiki ❤️"
+    wordlist+=" $(bee commands)"
+    COMPREPLY=($(compgen -W "${wordlist}" "${word}"))
+  else
+    case "${word}" in
+      "plugins" | "commands" | "version" | "update" | "wiki" | ❤️)
+        ;;
+      "help")
+        if [[ $COMP_CWORD -eq 2 ]]; then
+          COMPREPLY=($(compgen -W "$(bee plugins)" "${word}"))
+        fi
+        ;;
+      "new" | "res")
+        COMPREPLY=($(compgen -W "$(bee plugins)" "${word}"))
+        ;;
+      *)
+        COMPREPLY=$(compgen -W "$(ls -a)" -- "${word}")
+        ;;
+    esac
+  fi
 }
 
 complete -F _bee_completions bee
