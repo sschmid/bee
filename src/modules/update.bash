7eb35392211a563e6b8bab67acf2acb3e6fbdb2c
Simon Schmid <mail@sschmid.com>
2021-08-10 02:05:37 +0200

    Extract update methods

--- src/modules/update.bash
+++ src/modules/update.bash
@@ -1,38 +1,11 @@
 # bee::help update | update bee to the latest version
 bee::update() {
-  while (($# > 0)); do
-    case "$1" in
-      --read-latest-version)
-        curl -fsSL "${BEE_LATEST_VERSION_PATH}"
-        return
-        ;;
-      --read-latest-version-cached)
-        mkdir -p "${BEE_CACHES_PATH}"
-        local -i last_ts now delta
-        local cache cache_file="${BEE_CACHES_PATH}/.bee_latest_version_cache"
-        [[ ! -f "${cache_file}" ]] && echo "0,0" > "${cache_file}"
-        now=$(date +%s)
-        cache="$(cat "${cache_file}")"
-        last_ts="${cache%%,*}"
-        delta=$((now - last_ts))
-        if ((delta > BEE_LATEST_VERSION_CACHE_EXPIRE)); then
-          local latest
-          latest="$(curl -fsSL "${BEE_LATEST_VERSION_PATH}")"
-          echo "${now},${latest}" > "${cache_file}"
-          echo "${latest}"
-        else
-          echo "${cache##*,}"
-        fi
-        return
-        ;;
-      --)
-        shift
-        break
-        ;;
-      *) break ;;
-    esac
-    shift
-  done
+  while (($# > 0)); do case "$1" in
+    --read-latest-version) bee::update::read_latest_version; return ;;
+    --read-latest-version-cached) bee::update::read_latest_version_cached; return ;;
+    --) shift; break ;;
+    *) break ;;
+  esac; shift; done
 
   if (($# == 0)); then
     pushd "${BEE_SYSTEM_HOME}" > /dev/null || exit 1
@@ -41,5 +14,28 @@ bee::update() {
     popd > /dev/null || exit 1
   else
     bee::usage
+  fi
+}
+
+bee::update::read_latest_version() {
+  curl -fsSL "${BEE_LATEST_VERSION_PATH}"
+}
+
+bee::update::read_latest_version_cached() {
+  mkdir -p "${BEE_CACHES_PATH}"
+  local -i last_ts now delta
+  local cache cache_file="${BEE_CACHES_PATH}/.bee_latest_version_cache"
+  [[ ! -f "${cache_file}" ]] && echo "0,0" > "${cache_file}"
+  now=$(date +%s)
+  cache="$(cat "${cache_file}")"
+  last_ts="${cache%%,*}"
+  delta=$((now - last_ts))
+  if ((delta > BEE_LATEST_VERSION_CACHE_EXPIRE)); then
+    local latest
+    latest="$(curl -fsSL "${BEE_LATEST_VERSION_PATH}")"
+    echo "${now},${latest}" > "${cache_file}"
+    echo "${latest}"
+  else
+    echo "${cache##*,}"
   fi
 }
