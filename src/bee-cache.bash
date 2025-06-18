d271f30e9fb54ec445d0ed9cc17954919349b9bc
Simon Schmid <mail@sschmid.com>
2025-06-18 15:54:38 +0200

    Rename to bee-cache.bats

--- src/bee-cache.bash
+++ src/bee-cache.bash
@@ -6,8 +6,13 @@
 main() {
   if (( $# )); then
     case "$1" in
-      --clear) rm -rf "${BEE_CACHE_PATH}${2:+"/$2"}" ;;
-      *) bee::source "bee-help"; exit 1 ;;
+      --clear)
+        rm -rf "${BEE_CACHE_PATH}${2:+"/$2"}"
+        ;;
+      *)
+        bee::source "bee-help"
+        exit 1
+        ;;
     esac
   else
     os_open "${BEE_CACHE_PATH}"
