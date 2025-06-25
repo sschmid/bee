607c8a4c1b748a622d8ef5121212de0dfd04f08b
Simon Schmid <mail@sschmid.com>
2025-06-25 15:55:10 +0200

    Simplify plugin_with_deps_on_deps

--- test/fixtures/plugins/plugin_with_deps_on_deps/1.0.0/plugin_with_deps_on_deps.bash
+++ test/fixtures/plugins/plugin_with_deps_on_deps/1.0.0/plugin_with_deps_on_deps.bash
@@ -1,16 +1,1 @@
 _plugin_log "# plugin_with_deps_on_deps 1.0.0 sourced"
-
-plugin_with_deps_on_deps() {
-  if (( $# > 0 )); then
-    # shellcheck disable=SC2145
-    echo "hello from plugin_with_deps_on_deps 1.0.0 - $@"
-  else
-    echo "hello from plugin_with_deps_on_deps 1.0.0"
-  fi
-}
-
-plugin_with_deps_on_deps::greet() {
-  echo "greeting from plugin_with_deps_on_deps 1.0.0"
-  plugin_with_deps::greet "$@"
-  plugin_2::greet "$@"
-}
